e830843a04a3b38e920ab2788d495acb788b64c7
Setha Hong <setha_hong77@hotmail.com>
2023-04-28 14:19:58 +0200

    def init pour la classe Musique

---
+++ musique.py
@@ -0,0 +1,7 @@
+class Musique:
+    def __init__(self, titre, artiste, immatriculation):
+        self.titre = titre
+        self.artiste = artiste
+        self.immatriculation = immatriculation
+
+        
