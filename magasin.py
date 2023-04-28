fb5b98d56c7ff8e13eaa201024f7080abd60dbe0
Setha Hong <setha_hong77@hotmail.com>
2023-04-28 14:32:11 +0200

    ajout des méthodes pour ajouter des musiques

--- magasin.py
+++ magasin.py
@@ -3,3 +3,11 @@ class Magasin:
         self.typeMusique = typeMusique
         self.listeVinyle = []
         self.listeDVD = []
+
+    # Méthode pour ajouter une musique dans la liste des vinyles
+    def ajouter_musique_vinyle(self, musique):
+        self.listeVinyle.append(musique)
+
+    # Méthode pour ajouter une musique dans la liste des vinyles
+    def ajouter_musique_dvd(self, musique):
+        self.listeDVD.append(musique)
