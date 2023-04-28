5be7fa5689265a495cc2505395551e4f0d447c7c
Setha Hong <setha_hong77@hotmail.com>
2023-04-28 14:43:05 +0200

    ajout des méthodes pour retire dans les listes

--- magasin.py
+++ magasin.py
@@ -8,6 +8,22 @@ class Magasin:
     def ajouter_musique_vinyle(self, musique):
         self.listeVinyle.append(musique)
 
-    # Méthode pour ajouter une musique dans la liste des vinyles
+    # Méthode pour ajouter une musique dans la liste des DVD
     def ajouter_musique_dvd(self, musique):
         self.listeDVD.append(musique)
+
+    # Méthode pour retirer une musique dans la liste des vinyles
+    def retirer_vinyles(self, musique):
+        if(musique in self.listeVinyle):
+            self.listeVinyle.remove(musique)
+            return True
+        else:
+            return False
+    # Méthode pour retirer une musique dans la liste des DVD   
+    def retirer_dvd(self, musique):
+        if(musique in self.listeDVD):
+            self.listeDVD.remove(musique)
+            return True
+        else:
+            return False
+
