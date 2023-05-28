137a027b1801917f20c7851fd3a3acc96875a45f
Setha Hong <setha_hong77@hotmail.com>
2023-05-28 23:07:07 +0200

    création de la fonction verifier_slash

--- modelMusique.py
+++ modelMusique.py
@@ -20,5 +20,15 @@ class ModelMusique(BaseModel):
              raise ValueError("L'immatriculation n'est pas une chaîne de caractère")
          return value
     
-    
+    @validator('immatriculation')
+    def verifier_slash(cls, value):
+        """Verifier que l'immatriculation possède ses 3 slashs au bonnes emplacements"""
+        slash1 = value[2]
+        slash2 = value[6]
+        slash3 = value[10]
+
+        if (slash1 != "/") and (slash2 != "/") and (slash3 != "/"):
+            raise ValueError("L'immatriculation ne possède pas les slashs")
+        return value
+
 
