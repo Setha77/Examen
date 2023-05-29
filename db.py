27d52f0ea1cd972f11c88de5001796dbec2c4bd1
Setha Hong <setha_hong77@hotmail.com>
2023-05-29 21:42:56 +0200

    création de la fonction database

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 import pymongo
 from pymongo import MongoClient
 
-def database(): 
+def database():
   client = MongoClient("mongodb+srv://sethahong77:9ONsYp6oH05vpR7m@cluster0.0lcafi0.mongodb.net/?retryWrites=true&w=majority")
-  db = "Musique"
-  collection = "MagasinMusique"
+  db = client.Musique
+  return db
