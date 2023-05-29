a67b8315035fb770df49785eb50c625aafcd6d84
Setha Hong <setha_hong77@hotmail.com>
2023-05-29 11:57:20 +0200

    ajout des fonctions ajouter et retirer

--- modelMagasin.py
+++ modelMagasin.py
@@ -4,9 +4,31 @@ from modelMusique import ModelMusique
 
 class ModelMagasin(BaseModel):
     typeMusique: str
-    liste_Vinyle: list[ModelMusique] = []
-    liste_DVD: list[ModelMusique] = []
+    liste_Vinyle: List[ModelMusique]
+    liste_DVD: List[ModelMusique]
 
-    @validator("liste_Vinyle")
-    def ajouter_Vinyle(cls, value):
-        
+    # Récupère les musiques dans la liste Vinyle
+    def get_Vinyle(self):
+        return self.liste_Vinyle
+    
+    # Récupère les musiques dans la liste DVD
+    def get_DVD(self):
+        return self.liste_DVD
+    
+    # Ajouter des musiques dans la liste Vinyle
+    def ajouter_Vinyle(self, musique: ModelMusique):
+        self.liste_Vinyle.append(musique)
+
+    # Ajouter des musiques dans la liste DVD
+    def ajouter_DVD(self, musique: ModelMusique):
+        self.liste_DVD.append(musique)
+    
+    # Enlever des musiques dans la liste Vinyle
+    def retirer_Vinyle(self, musique: ModelMusique):
+        if musique in self.liste_Vinyle:
+            self.liste_Vinyle.remove(musique)
+
+    # Enlever des musiques dans la liste Vinyle
+    def retirer_DVD(self, musique: ModelMusique):
+        if musique in self.liste_DVD:
+            self.liste_DVD.remove(musique)
