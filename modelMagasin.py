5c2289c8fc6e4597011ad0e26c1a50969969f5e9
Setha Hong <setha_hong77@hotmail.com>
2023-05-29 16:52:12 +0200

    changement dans les méthodes add

--- modelMagasin.py
+++ modelMagasin.py
@@ -28,15 +28,17 @@ class ModelMagasin(BaseModel):
         
     # Ajouter des musiques dans la liste Vinyle
     def ajouter_Vinyle(self, musique: ModelMusique):
+        """
         compatible = self.verifier_typeMusique(musique)
         if compatible == 1:
-            self.liste_Vinyle.append(musique)
+        """
+        self.liste_Vinyle.append(musique)
 
     # Ajouter des musiques dans la liste DVD
     def ajouter_DVD(self, musique: ModelMusique):
-        compatible = self.verifier_typeMusique(musique)
-        if compatible == 1:
-            self.liste_DVD.append(musique)
+        #compatible = self.verifier_typeMusique(musique)
+        #if compatible == 1:
+        self.liste_DVD.append(musique)
     
     # Enlever des musiques dans la liste Vinyle
     def retirer_Vinyle(self, musique: ModelMusique):
--- testmodelMagasin.py
+++ testmodelMagasin.py
@@ -2,67 +2,76 @@ import unittest
 from mock import Mock
 from modelMagasin import ModelMagasin, ModelMagasinException
 
-# Test si ajouter musique dans Vinyle fonctionne
-def test_add_musiqueVinyle():
-    musique = Mock()
-    modelMag = ModelMagasin()
-    modelMag.ajouter_Vinyle(musique)
+class TestModelMagasin(unittest.TestCase):
+    def setUp(self):
+        musique = Mock()
+        self.magasin = ModelMagasin(
+            typeMusique = "POP",
+            liste_Vinyle = [],
+            liste_DVD = [], 
+        )
 
-    assert modelMag.liste_Vinyle == [musique]
-
-# Test si ajouter musique dans DVD fonctionne
-def test_add_musiqueDVD():
-    musique = Mock()
-    modelMag = ModelMagasin()
-    modelMag.ajouter_DVD(musique)
-
-    assert modelMag.liste_DVD == [musique]
-
-# Test si retirer une musique dans Vinyle fonctionne
-def test_remove_musiqueVinyle():
-    musique = Mock()
-    modelMag = ModelMagasin()
-    modelMag.liste_Vinyle = [musique]
-    modelMag.retirer_Vinyle(musique)
-
-    assert modelMag.liste_Vinyle == []
-
-# Test si retirer une musique dans DVD fonctionne
-def test_remove_musiqueDVD():
-    musique = Mock()
-    modelMag = ModelMagasin()
-    modelMag.liste_DVD = [musique]
-    modelMag.retirer_DVD(musique)
-
-    assert modelMag.liste_DVD == []
-
-# Test si retirer une musique inexistante dans Vinyle 
-def test_remove_musiqueVinyleFailure():
-    musique = Mock()
-    other_musique = Mock()
-    modelMag = ModelMagasin()
-    modelMag.liste_Vinyle = [musique]
-
-    try:
-        modelMag.retirer_Vinyle(other_musique)
-    except ModelMagasinException:
-        pass
-    else:
-        raise Exception("expected Exception was not raised")
+    # Test si ajouter musique dans Vinyle fonctionne
+    def test_add_musiqueVinyle(self):
+        musique = Mock()
+        #modelMag = ModelMagasin()
+        self.magasin.ajouter_Vinyle(musique)
+        self.assertEqual(self.magasin.liste_Vinyle, [musique])
+        #assert self.magasin.liste_Vinyle == [musique]
     
-# Test si retirer une musique inexistante dans DVD
-def test_remove_musiqueDVDFailure():
-    musique = Mock()
-    other_musique = Mock()
-    modelMag = ModelMagasin()
-    modelMag.liste_Vinyle = [musique]
-
-    try:
-        modelMag.retirer_DVD(other_musique)
-    except ModelMagasinException:
-        pass
-    else:
-        raise Exception("expected Exception was not raised")
+    # Test si ajouter musique dans DVD fonctionne
+    def test_add_musiqueDVD(self):
+        musique = Mock()
+        #modelMag = ModelMagasin()
+        self.magasin.ajouter_DVD(musique)
+        self.assertEqual(self.magasin.liste_DVD, [musique])
+        #assert self.magasin.liste_DVD == [musique]
     
+    # Test si retirer une musique dans Vinyle fonctionne
+    def test_remove_musiqueVinyle(self):
+        musique = Mock()
+        #modelMag = ModelMagasin()
+        self.magasin.liste_Vinyle = [musique]
+        self.magasin.retirer_Vinyle(musique)
+
+        assert self.magasin.liste_Vinyle == []
+    
+    # Test si retirer une musique dans DVD fonctionne
+    def test_remove_musiqueDVD(self):
+        musique = Mock()
+        #modelMag = ModelMagasin()
+        self.magasin.liste_DVD = [musique]
+        self.magasin.retirer_DVD(musique)
+
+        assert self.magasin.liste_DVD == []
+
+    # Test si retirer une musique inexistante dans Vinyle 
+    def test_remove_musiqueVinyleFailure(self):
+        musique = Mock()
+        other_musique = Mock()
+        #modelMag = ModelMagasin()
+        self.magasin.liste_Vinyle = [musique]
+
+        try:
+            self.magasin.retirer_Vinyle(other_musique)
+        except ModelMagasinException:
+            pass
+        else:
+            raise Exception("expected Exception was not raised")
+        
+    # Test si retirer une musique inexistante dans DVD
+    def test_remove_musiqueDVDFailure(self):
+        musique = Mock()
+        other_musique = Mock()
+        #modelMag = ModelMagasin()
+        self.magasin.liste_Vinyle = [musique]
+
+        try:
+            self.magasin.retirer_DVD(other_musique)
+        except ModelMagasinException:
+            pass
+        else:
+            raise Exception("expected Exception was not raised")
+
 if __name__ == '__main__':
     unittest.main()
