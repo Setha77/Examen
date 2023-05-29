428e9b9967f0ad31538b3d7b443e9bbb75a78941
Setha Hong <setha_hong77@hotmail.com>
2023-05-29 20:39:09 +0200

    création de db pour se connecter à MongoDB

---
+++ db.py
@@ -0,0 +1,5 @@
+import pymongo
+from pymongo import MongoClient
+client = "mongodb+srv://sethahong77:9ONsYp6oH05vpR7m@cluster0.0lcafi0.mongodb.net/?retryWrites=true&w=majority"
+db = "Musique"
+collection = "MagasinMusique"
